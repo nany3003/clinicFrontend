0fc8b43970ecbc4f1e21f1f5e90f33f6c0080058
Nasser <abdo.abdo3003@gmail.com>
2023-12-25 12:56:58 -0500

    installed Charka UI

--- src/App.tsx
+++ src/App.tsx
@@ -1,38 +1,6 @@
-import Button from "./components/Button.tsx";
-import ListGroup from "./components/ListGroup.tsx";
-import Alert from "./components/Alert.tsx";
-import { useState } from "react";
-
+import { Button, ButtonGroup } from "@chakra-ui/react";
 function App() {
-  const [alertVisible, setAlertVisiblity] = useState(false);
-  const list = ["Egypt", "United Stated Of America", "Morocco"];
-  const onSelectItem = (item: string) => {
-    console.log(item);
-  };
-  const onClick = () => {
-    console.log("Button clicked");
-    setAlertVisiblity(true);
-  };
-  return (
-    <div>
-      <ListGroup
-        items={list}
-        heading={"Counteries"}
-        onSelectItem={onSelectItem}
-      />
-      <Button color={"primary"} onClick={onClick}>
-        Register
-      </Button>
-      {alertVisible && (
-        <Alert
-          title="This is an Alert"
-          description="You have been Alerted"
-          color="danger"
-          onClose={() => setAlertVisiblity(false)}
-        />
-      )}
-    </div>
-  );
+  return <Button colorScheme="blue">Button</Button>;
 }
 
 export default App;
